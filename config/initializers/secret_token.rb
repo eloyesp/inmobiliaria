c63f8adfb8eaea290289064eb310481d8c41d579
Eloy <eloyesp@gmail.com>
2011-06-22 18:19:06 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inmobiliaria::Application.config.secret_token = '3a3f48007a363f252bcfa87239e9502568babe5a85c4222a5bce81297c00f36435ffea01483868f0bb930f303cc01e7187cd861c24c56895766f49f49e5ba2cb'
